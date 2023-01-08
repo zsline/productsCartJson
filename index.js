c24ef72a1e3ab9408a59376d1fcc39ed1942535a
zsline <sergei.dereka@gmail.com>
2023-01-08 20:29:48 +0200

    res

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function render() {
     productsPage.render();
 }
 let CATALOG = [];
-fetch('https://zsline.github.io/productsCartJson/server/catalog.json')
+fetch('https://gist.githubusercontent.com/zsline/88ea60c143155c52e07f8e564303d22a/raw/6cba0e20e0f8d2881f331d3f5c357f79fc138d87/catalog.json')
 .then(res => res.json())
 .then(body => {
     CATALOG = body;
